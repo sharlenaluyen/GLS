5fb57e1b79fa2916276349e382821657cd7ad730
Josh Goldberg <josh@fullscreenmario.com>
2016-04-25 12:01:17 -0400

    Fixed `[][]` in arrays
    Surprisingly simple. TypeCommand::convertArrayType was assuming there
    was only one `[]`.

--- Source/Commands/TypeCommand.ts
+++ Source/Commands/TypeCommand.ts
@@ -73,7 +73,7 @@ namespace GLS.Commands {
             let bracketIndex: number = typeNameRaw.indexOf("["),
                 typeName: string = this.convertType(typeNameRaw.substring(0, bracketIndex));
 
-            return typeName + "[]";
+            return typeName + typeNameRaw.substring(bracketIndex);
         }
 
         /**
