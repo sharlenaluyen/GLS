9b27fbd16a8f477af992c6c8c22d720cc49a4b20
Josh Goldberg <josh@fullscreenmario.com>
2016-04-25 12:45:13 -0400

    Added end-to-end tests for for numbers loop
    TypeScript isn't adding the `let`?

---
+++ Tests/EndToEnd/Loops/for numbers.cs
@@ -0,0 +1,16 @@
+-
+for (int aaa = 0; aaa < 10; aaa += 1)
+{
+    // ...
+}
+
+for (float bbb = 0.0; bbb < 10.0; bbb += 1)
+{
+    // ...
+}
+
+for (double ccc = 0.0; ccc < 10.0; ccc += 1)
+{
+    // ...
+}
+-
---
+++ Tests/EndToEnd/Loops/for numbers.ts
@@ -0,0 +1,13 @@
+-
+for (let aaa: number = 0; aaa < 10; aaa += 1) {
+    // ...
+}
+
+for (let bbb: number = 0.0; bbb < 10.0; bbb += 1) {
+    // ...
+}
+
+for (let ccc: number = 0.0; ccc < 10.0; ccc += 1) {
+    // ...
+}
+-
