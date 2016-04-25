46062429807ff98497f2f934a2cd4469589e92d7
Josh Goldberg <josh@fullscreenmario.com>
2016-04-25 11:59:34 -0400

    Added end-to-end tests for arrays
    Oh no, `[][]` is broken!

---
+++ Tests/EndToEnd/Arrays/arrays.py
@@ -0,0 +1,13 @@
+-
+# Initialization
+aaa = []
+bbb = [1, 2, 3]
+ccc = [aaa, ["eee", "fff"]]
+
+# Members
+aaa[0]
+aaa[1 + 1]
+
+# Length
+len(aaa)
+-
---
+++ Tests/EndToEnd/Arrays/arrays.ts
@@ -0,0 +1,13 @@
+-
+// Initialization
+let aaa: string[] = [];
+let bbb: number[] = [1, 2, 3];
+let ccc: string[][] = [aaa, ["eee", "fff"]];
+
+// Members
+aaa[0]
+aaa[1 + 1]
+
+// Length
+aaa.length;
+-
